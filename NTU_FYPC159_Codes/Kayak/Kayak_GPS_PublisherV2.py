29d3b9d5f554791ca3169ef00f0a6f3ea38ec824
SSIFYP <144475079+SSIFYP@users.noreply.github.com>
2024-03-21 18:25:16 +0800

    Add files via upload

---
+++ NTU_FYPC159_Codes/Kayak/Kayak_GPS_PublisherV2.py
@@ -0,0 +1,107 @@
+import serial
+import math
+from ublox_gps import UbloxGps
+import time
+from time import sleep, strftime
+import paho.mqtt.publish as publish
+import sys
+import threading
+
+port = serial.Serial('/dev/serial0', baudrate=38400, timeout=1)
+gps = UbloxGps(port)
+c_lat = 0.0
+c_lon = 0.0
+p_lat = 0.0
+p_lon = 0.0
+Distancebtwn2 = 0.00
+dist_all = 150.00
+t_dist2 = 0.000
+
+MQTT_SERVER= "192.168.1.1"
+MQTT_PORT = 1883
+MQTT_PATH = "boat_GPS"
+
+counter = 0.0 
+
+
+def haversine(plat, plon, clat, clon):
+    E_radius = 6378 * 1000 * 100
+    plat_rad = math.radians(plat)
+    plon_rad = math.radians(plon)
+    clat_rad = math.radians(clat)
+    clon_rad = math.radians(clon)
+    dlat = clat_rad  - plat_rad
+    dlon = clon_rad - plon_rad
+    hvs = math.sin(dlat * 0.5)*math.sin(dlat * 0.5) +math.sin(dlon * 0.5)*math.sin(dlon* 0.5)*math.cos(plat_rad) *math.cos(clat_rad);
+    haversine_dist = math.asin(math.sqrt(hvs))*E_radius*2.0
+    return haversine_dist    
+    '''
+    #Eculidean, is not as good as i've imagined 
+    E_radius = 6378 * (1000) * (100)
+    plat_rad = math.radians(plat)
+    plon_rad = math.radians(plon)
+    clat_rad = math.radians(clat)
+    clon_rad = math.radians(clon)
+    x1 = E_radius * math.sin(plat_rad) * math.cos(plon_rad)
+    y1 = E_radius * math.cos(plat_rad) * math.sin(plon_rad)
+    x2 = E_radius * math.sin(clat_rad) * math.cos(clon_rad)
+    y2 = E_radius * math.cos(clat_rad) * math.sin(clon_rad)
+    distanceEcu = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return distanceEcu'''
+
+def GPSR():
+    global c_lat, c_lon , p_lat, p_lon, t_dist2 , Distancebtwn2 , counter
+    start_time = time.time()
+    while True:
+        try:
+            print("Listening for UBX Messages")
+            with open("/home/ssi/230224_gps_Near_River_Test.csv", "a") as log:
+                geo = gps.geo_coords()
+                print("Longitude: ", geo.lon)
+                print("Latitude: ", geo.lat)
+                print("Heading of Motion: ", geo.headMot)
+                print("Total Distance : ", t_dist2)
+                if geo.lat != 0 and geo.lon != 0:
+                    if p_lat is not None and p_lon is not None:
+                        c_lat = geo.lat
+                        c_lon = geo.lon
+                        Distancebtwn2 = haversine(p_lat, p_lon, c_lat, c_lon)
+
+                        if Distancebtwn2 > dist_all:
+                            p_lat = c_lat
+                            p_lon = c_lon
+                            t_dist2 += Distancebtwn2
+
+
+                        log.write("{0},{1},{2},{3},{4},{5},{6}\n".format(
+                            strftime("%Y-%m-%d %H:%M:%S"),
+                            str(p_lat), str(p_lon),
+                            str(c_lat), str(c_lon),
+                            str(Distancebtwn2), str(t_dist2)))
+                    else:
+                        p_lat = geo.lat
+                        p_lon = geo.lon
+            counter = counter+1.0
+            print(counter / (time.time() - start_time) )
+        except (ValueError, IOError) as err:
+            print(err)
+            sleep(1)  # Sleep for a while before retrying
+
+        except Exception as e:
+            print("An unexpected error occurred:", str(e))
+            sleep(1)  # Sleep for a while before retrying
+def MQTT():
+        global p_lat, p_lon ,c_lat ,c_lon, Distancebtwn2 , t_dist2
+        while True:
+            #print("GPS running")
+            message = f"{p_lat}, {p_lon},{c_lat}, {c_lon},{Distancebtwn2} , {t_dist2}"
+            publish.single(MQTT_PATH , message , hostname = MQTT_SERVER, port = MQTT_PORT)
+
+
+
+t1 = threading.Thread(target = GPSR)
+t2 = threading.Thread(target = MQTT)
+t1.start()
+t2.start()
+t1.join
+t2.join()
